055c9a93003b90f52445cd0ea0d1499930d1dc33
Jun Taek Lee <juntaek1996@gmail.com>
2017-03-09 02:12:40 -0600

    added week4sigcostplot

--- assets/R/week4.R
+++ assets/R/week4.R
@@ -3,8 +3,8 @@ require(mosaic)
 setwd("~/Documents/Projects/Website/juntaeklee/assets/R/")
 
 #Sigmoid function
-x <- c(-100:100)/10
-y <- 1.0/(1+exp(-x))
+X <- c(-100:100)/10
+y <- 1.0/(1+exp(-X))
 dfsigmoid <- data.frame(x,y)
 ggplot(data = dfsigmoid, aes(x=x, y=y)) + 
     geom_line()  + 
@@ -17,8 +17,8 @@ ggplot(data = dfsigmoid, aes(x=x, y=y)) +
          y = "sig(x)") 
 
 #using linear cost function for logistic regression
-X <- data.frame("x0" = rep(1,200), "x1" = rnorm(200,10,1))
-y <- sapply(x$x1, function(x) rnorm(1,x,5))
+X <- data.frame("x0" = rep(1,200), "x1" = rnorm(200,0,1), "x1^2" = rnorm(200,0,1)^2)
+y <- sapply(X$x1, function(x) rnorm(1,x,1))
 sigmoid <- function(x) {
     1.0/(1+exp(-x))
 }
@@ -27,17 +27,42 @@ lincostf <- function(X,y,theta) {
     1/(2*m)*sum(((as.matrix(X) %*% as.matrix(theta))-y)^2)
 }
 
-costs <- sapply(c(-100:100), function(x) lincostf(X,y,c(0,x)))
-index <- c(1:length(costs))
+costs <- sapply(c(-100:100), function(x) lincostf(X,y,c(0,x,x)))
+index <- c(-100:100)
 dflincosts <- data.frame(costs,index)
 
 #week4lincostplot
 ggplot(data = dflincosts, aes(x=index, y=costs)) + 
-    geom_point()  + 
+    geom_line()  + 
     theme(legend.position="none",
           plot.title = element_text(hjust = 0.5,
                                     face = 'bold',
                                     size = 16)) + 
     labs(title="Linear Regression Cost Function",
+         x = "Theta",
+         y = "Cost Function")
+
+
+lincostfs <- function(X,y,theta) {
+    m <- length(y)
+    1/(2*m)*sum((sigmoid((as.matrix(X) %*% as.matrix(theta)))-y)^2)
+}
+ysig <- sigmoid(y)
+ysig[ysig >= 0.5] <- 1
+ysig[ysig < 0.5] <- 0
+ysig <- sample(x = c(0,1),200,replace=T)
+Xsig <- data.frame("x0" = X$x0,"x1" = X$x1,"x2" = X$x1^2, "x3" = X$x1^3)
+costs <- sapply(c(-100:100), function(x) lincostfs(Xsig,ysig,c(0,x,1,1)))
+index <- c(-100:100)
+dfsigcosts <- data.frame(costs,index)
+
+#week4lincostplot
+ggplot(data = dfsigcosts, aes(x=index, y=costs)) + 
+    geom_line()  + 
+    theme(legend.position="none",
+          plot.title = element_text(hjust = 0.5,
+                                    face = 'bold',
+                                    size = 16)) + 
+    labs(title="Linear Regression Cost Function with Sigmoid",
          x = "Theta",
          y = "Cost Function")
